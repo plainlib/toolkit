96d3a2cc9f9b51991e5d95127cc76fd571d4f6eb
Alexander Tverskoy <astverskoy@gmail.com>
2026-08-26 21:02:31 +0300

    Improved the oneshottimer

--- oneshottimer.pas
+++ oneshottimer.pas
@@ -3,6 +3,17 @@
 //  Licensed under the MIT License
 //  You may obtain a copy of the License at https://opensource.org/licenses/MIT
 //-----------------------------------------------------------------------------------
+//  Module for delayed one-shot procedure calls via a timer.
+//
+//  Key features:
+//  1. SetTimeout(Delay, Callback) - call a procedure without parameters.
+//  2. SetTimeout(Delay, Callback, Data: Pointer) - pass an arbitrary pointer.
+//  3. SetTimeout(Delay, Callback, Args: array of const) - pass a list of mixed-type parameters.
+//
+//  In all variants you can get a timer reference (for cancellation) via out parameter.
+//  Cancellation is done by ClearTimeout(var Timer).
+//  All timers are freed automatically; calling ClearTimeout twice is safe.
+//-----------------------------------------------------------------------------------
 
 unit OneShotTimer;
 
@@ -15,16 +26,26 @@ uses
 
 type
   PTimer = ^TTimer;
+  // Original callback without parameters
   TOneShotCallback = procedure of object;
+  // Callback that receives a Pointer to user data
+  TOneShotCallbackWithData = procedure(Data: Pointer) of object;
+  // Callback that receives an array of const (multiple typed parameters)
+  TOneShotArrayCallback = procedure(const Args: array of const) of object;
 
-// Schedules a one-time callback after the specified delay.
-// Call with two arguments to run without cancellation.
-// Call with three arguments to get a timer reference for cancellation.
+// Existing overloads (no data)
 procedure SetTimeout(Delay: cardinal; Callback: TOneShotCallback); overload;
 procedure SetTimeout(out Timer: TTimer; Delay: cardinal; Callback: TOneShotCallback); overload;
 
-// Cancels a pending timer obtained from SetTimeout.
-// Safe to call even if the timer has already fired or is nil.
+// Existing overloads with Pointer data
+procedure SetTimeout(Delay: cardinal; Callback: TOneShotCallbackWithData; Data: Pointer); overload;
+procedure SetTimeout(out Timer: TTimer; Delay: cardinal; Callback: TOneShotCallbackWithData; Data: Pointer); overload;
+
+// New overloads with array of const
+procedure SetTimeout(Delay: cardinal; Callback: TOneShotArrayCallback; const Args: array of const); overload;
+procedure SetTimeout(out Timer: TTimer; Delay: cardinal; Callback: TOneShotArrayCallback; const Args: array of const); overload;
+
+// Cancels a pending timer
 procedure ClearTimeout(var Timer: TTimer);
 
 implementation
@@ -36,43 +57,130 @@ type
   TOneShotTimer = class(TTimer)
   private
     FCallback: TOneShotCallback;
+    FCallbackWithData: TOneShotCallbackWithData;
+    FArrayCallback: TOneShotArrayCallback;
     FUserVar: ^TTimer;
+    FData: Pointer;
+    FArgs: array of TVarRec; // Copy of array of const
+    procedure CopyArgs(const A: array of const);
+    procedure ClearArgs;
   public
     constructor CreateWith(ADelay: cardinal; ACallback: TOneShotCallback; AUserVar: PTimer);
+    constructor CreateWithData(ADelay: cardinal; ACallback: TOneShotCallbackWithData; AUserVar: PTimer; AData: Pointer);
+    constructor CreateWithArray(ADelay: cardinal; ACallback: TOneShotArrayCallback; AUserVar: PTimer; const A: array of const);
     destructor Destroy; override;
     procedure TimerFire(Sender: TObject);
   end;
 
+// Helper to deep copy TVarRec array (supports common simple types)
+procedure TOneShotTimer.CopyArgs(const A: array of const);
+var
+  i: Integer;
+begin
+  SetLength(FArgs, Length(A));
+  for i := 0 to High(A) do
+  begin
+    FArgs[i] := A[i];
+    // For string types we need to copy the string content,
+    // because the original may be destroyed after the call.
+    case FArgs[i].VType of
+      vtAnsiString:
+        AnsiString(FArgs[i].VAnsiString) := AnsiString(A[i].VAnsiString);
+      vtUnicodeString:
+        UnicodeString(FArgs[i].VUnicodeString) := UnicodeString(A[i].VUnicodeString);
+      vtWideString:
+        WideString(FArgs[i].VWideString) := WideString(A[i].VWideString);
+      vtInterface: begin
+        // Save the pointer and increase reference count
+        FArgs[i].VInterface := A[i].VInterface;
+        if FArgs[i].VInterface <> nil then
+          IInterface(FArgs[i].VInterface)._AddRef;
+      end;
+      // For other types (integers, booleans, chars, floats) value is stored directly
+    end;
+  end;
+end;
+
+procedure TOneShotTimer.ClearArgs;
+var
+  i: Integer;
+begin
+  for i := 0 to High(FArgs) do
+  begin
+    case FArgs[i].VType of
+      vtAnsiString: AnsiString(FArgs[i].VAnsiString) := '';
+      vtUnicodeString: UnicodeString(FArgs[i].VUnicodeString) := '';
+      vtWideString: WideString(FArgs[i].VWideString) := '';
+      vtInterface:
+        if FArgs[i].VInterface <> nil then
+          IInterface(FArgs[i].VInterface)._Release;
+    end;
+  end;
+  SetLength(FArgs, 0);
+end;
+
 constructor TOneShotTimer.CreateWith(ADelay: cardinal; ACallback: TOneShotCallback; AUserVar: PTimer);
 begin
-  // No owner - timer is managed via the global list and freed manually
   inherited Create(nil);
   FCallback := ACallback;
+  FCallbackWithData := nil;
+  FArrayCallback := nil;
   FUserVar := AUserVar;
+  FData := nil;
   Interval := ADelay;
   OnTimer := @TimerFire;
   Enabled := True;
   if FUserVar <> nil then
     FUserVar^ := Self;
-  // Add to global list for guaranteed cleanup
+  FOneShotTimers.Add(Self);
+end;
+
+constructor TOneShotTimer.CreateWithData(ADelay: cardinal; ACallback: TOneShotCallbackWithData; AUserVar: PTimer; AData: Pointer);
+begin
+  inherited Create(nil);
+  FCallback := nil;
+  FCallbackWithData := ACallback;
+  FArrayCallback := nil;
+  FUserVar := AUserVar;
+  FData := AData;
+  Interval := ADelay;
+  OnTimer := @TimerFire;
+  Enabled := True;
+  if FUserVar <> nil then
+    FUserVar^ := Self;
+  FOneShotTimers.Add(Self);
+end;
+
+constructor TOneShotTimer.CreateWithArray(ADelay: cardinal; ACallback: TOneShotArrayCallback; AUserVar: PTimer; const A: array of const);
+begin
+  inherited Create(nil);
+  FCallback := nil;
+  FCallbackWithData := nil;
+  FArrayCallback := ACallback;
+  FUserVar := AUserVar;
+  FData := nil;
+  CopyArgs(A); // Store a deep copy of the arguments
+  Interval := ADelay;
+  OnTimer := @TimerFire;
+  Enabled := True;
+  if FUserVar <> nil then
+    FUserVar^ := Self;
   FOneShotTimers.Add(Self);
 end;
 
 destructor TOneShotTimer.Destroy;
 begin
-  // Remove from global list
   FOneShotTimers.Remove(Self);
-  // Clear external reference if still valid (may be nil if already cleared)
   if FUserVar <> nil then
     FUserVar^ := nil;
   FUserVar := nil;
+  ClearArgs; // Free copied strings/interfaces
   inherited Destroy;
 end;
 
 procedure TOneShotTimer.TimerFire(Sender: TObject);
 begin
   Enabled := False;
-  // Immediately nil the external variable so no one touches a dead object
   if FUserVar <> nil then
   begin
     FUserVar^ := nil;
@@ -80,13 +188,17 @@ begin
   end;
   try
     if Assigned(FCallback) then
-      FCallback();
+      FCallback()
+    else if Assigned(FCallbackWithData) then
+      FCallbackWithData(FData)
+    else if Assigned(FArrayCallback) then
+      FArrayCallback(FArgs); // Pass the stored args
   finally
-    // Free immediately; global list will remove it in destructor
-    Free;
+    Free; // Destructor will clear args
   end;
 end;
 
+// Existing implementations...
 procedure SetTimeout(Delay: cardinal; Callback: TOneShotCallback);
 begin
   TOneShotTimer.CreateWith(Delay, Callback, nil);
@@ -96,6 +208,28 @@ procedure SetTimeout(out Timer: TTimer; Delay: cardinal; Callback: TOneShotCallb
 begin
   Timer := nil;
   TOneShotTimer.CreateWith(Delay, Callback, @Timer);
+end;
+
+procedure SetTimeout(Delay: cardinal; Callback: TOneShotCallbackWithData; Data: Pointer);
+begin
+  TOneShotTimer.CreateWithData(Delay, Callback, nil, Data);
+end;
+
+procedure SetTimeout(out Timer: TTimer; Delay: cardinal; Callback: TOneShotCallbackWithData; Data: Pointer);
+begin
+  Timer := nil;
+  TOneShotTimer.CreateWithData(Delay, Callback, @Timer, Data);
+end;
+
+procedure SetTimeout(Delay: cardinal; Callback: TOneShotArrayCallback; const Args: array of const);
+begin
+  TOneShotTimer.CreateWithArray(Delay, Callback, nil, Args);
+end;
+
+procedure SetTimeout(out Timer: TTimer; Delay: cardinal; Callback: TOneShotArrayCallback; const Args: array of const);
+begin
+  Timer := nil;
+  TOneShotTimer.CreateWithArray(Delay, Callback, @Timer, Args);
 end;
 
 procedure ClearTimeout(var Timer: TTimer);
@@ -115,10 +249,8 @@ procedure FreeAllOneShotTimers;
 var
   i: Integer;
 begin
-  // Prevent destructor from writing to possibly destroyed external variables
   for i := 0 to FOneShotTimers.Count - 1 do
     TOneShotTimer(FOneShotTimers[i]).FUserVar := nil;
-  // Free all remaining timers
   while FOneShotTimers.Count > 0 do
     TOneShotTimer(FOneShotTimers[0]).Free;
 end;
